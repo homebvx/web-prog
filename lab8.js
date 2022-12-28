3f0dafc0f98a3daf2e9da878d292f374f1091093
homebvx <80848812+homebvx@users.noreply.github.com>
2022-12-28 21:59:03 +0700

    JS количество дней с ДР

--- lab8.js
+++ lab8.js
@@ -3,3 +3,13 @@ function showDate(params) {
     let today = new Date();
     out.innerHTML = today.toLocaleDateString('ru-Ru');
 }
+
+function showDaysCount(params) {
+    let today = new Date();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil(daysCount);
+    result.innerHTML = 'С даты рождения прошло дней: ' + daysCount;
+}
