cb8ddd527b621fff95770189ff9ec3ecc6d499e4
homebvx <80848812+homebvx@users.noreply.github.com>
2022-12-28 22:01:35 +0700

    JS круглая дата

--- lab8.js
+++ lab8.js
@@ -23,3 +23,17 @@ function showTime() {
 
 window.addEventListener('load', showTime);
 setInterval(showTime, 1000);
+
+function showNewDate(params) {
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date(inputDate.value);
+    
+    
+    let days = document.querySelector('input[type=number]').value;
+
+    let NewDate = birthday.getTime() + days*24*60*60*1000;
+    NewDate = new Date(NewDate);
+   
+    resdays.innerHTML = 'Круглая дата:  ' + NewDate;
+}
+
