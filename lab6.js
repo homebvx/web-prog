230446713951d8f42e39b0005ab1e2c072fc17af
homebvx <80848812+homebvx@users.noreply.github.com>
2022-12-28 20:15:44 +0700

    JS Переводы

---
+++ lab6.js
@@ -0,0 +1,16 @@
+function setRussian(){
+  let name = document.getElementById('student');
+  name.innerHTML = 'Прокина Ольга';
+  let footer = document .getElementById ('footer');
+footer.innerHTML = 'Факультет бизнеса, ФБИ-00, 3 курс, 2022;
+  document.getElementById ('your-name').innerHTML = 'Ваше имя';
+  document.getElementById ('input-name').placeholder = 'Введите имя';
+  
+  function setEnglish() {
+let name = document.getElementById ('student');
+name.innerHTML = 'Prokina Olga;
+    let footer = document.getElementById('footer');
+footer.innerHTML - 'Faculty of Business, FBI-00, 3rd year, 2022';
+   document.getElementById ('your-name') .innerHTML = 'Your name';
+document.getElementById ('input-name') .placeholder = 'Enter your name';
+  
