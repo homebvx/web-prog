fdca955da6dff09d23d9fa65ac713a67c1020b2f
homebvx <80848812+homebvx@users.noreply.github.com>
2022-12-28 21:17:57 +0700

    JS лесенка

--- lab7.js
+++ lab7.js
@@ -21,3 +21,17 @@ let g = Math.round (Math.random () *255);
 let b = Math.round (Math.random () *255);
 let color = 'rgb(' + r + 1 + g + b + ')';
 document.getElementById('user-data').style.backgroundColor = color; }
+
+function makeStairs() {
+let stairs = document.querySelector('#stairs');
+let stepCount = 20;
+for(let i = 0; i<stepCount; i++) {
+stairs.innerHTML += '<div class="step"></div>';
+}
+
+let steps = document.querySelectorAll(' .step');
+for(let i = 0; i<steps.length; i++) {
+steps[i].style.left = (30 + 10*i) + 'px';
+steps[i].style.top = (300 + 80*i) + 'px';
+steps[i].innerHTML = 'Блок номер' + i;
+}
